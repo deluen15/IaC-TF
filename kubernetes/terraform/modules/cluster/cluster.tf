853e0a54015e79a9a39fd6b3f35b80f2b86b1fa9
Endrit <endrit.zeqo@19bytes.de>
2022-04-21 18:04:37 +0200

    check terraform

--- kubernetes/terraform/modules/cluster/cluster.tf
+++ kubernetes/terraform/modules/cluster/cluster.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster" "IaC-aks-cluster" {
   location            = azurerm_resource_group.IaC-aks-test.location
   resource_group_name = azurerm_resource_group.IaC-aks-test.name
   dns_prefix          = "IaC-aks-cluster"
-  kubernetes_version  = var.kubernetes_version
+#  kubernetes_version  = var.kubernetes_version
 
   default_node_pool {
     name       = "default"
