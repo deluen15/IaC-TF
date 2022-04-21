9d167ce85538c846d8c7985e614c58e3daea9a6c
Endrit <endrit.zeqo@19bytes.de>
2022-04-21 18:28:24 +0200

    check terraform

--- kubernetes/terraform/modules/cluster/cluster.tf
+++ kubernetes/terraform/modules/cluster/cluster.tf
@@ -16,10 +16,10 @@ resource "azurerm_kubernetes_cluster" "IaC-aks-cluster" {
     type       = "VirtualMachineScaleSets"
   }
 
-  #  service_principal {
-  #    client_id     = var.serviceprinciple_id
-  #    client_secret = var.serviceprinciple_key
-  #  }
+    service_principal {
+      client_id     = "$(ARM_CLIENT_ID)"
+      client_secret = "$(ARM_CLIENT_SECRET)"
+    }
   network_profile {
     network_plugin = "kubenet"
   }
