d30adae63f6a16806d129714542da4cd8583fcf0
Endrit <endrit.zeqo@19bytes.de>
2022-04-22 09:50:48 +0200

    check terraform

--- kubernetes/terraform/main.tf
+++ kubernetes/terraform/main.tf
@@ -13,10 +13,10 @@ terraform {
   }
 }
 provider "azurerm" {
-  subscription_id = var.subscription_id
-  client_id       = var.serviceprinciple_id
-  client_secret   = var.serviceprinciple_key
-  tenant_id       = var.tenant_id
+  #  subscription_id = var.subscription_id
+  #  client_id       = var.serviceprinciple_id
+  #  client_secret   = var.serviceprinciple_key
+  #  tenant_id       = var.tenant_id
   features {}
 }
 
